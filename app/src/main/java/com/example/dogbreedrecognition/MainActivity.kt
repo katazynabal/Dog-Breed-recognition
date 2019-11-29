f18bf92e0d286e456562602083c763c2d919edc3
Aurimas Sikorkas <aurimas@dogo.app>
2019-11-29 12:24:36 +0200

    a

--- app/src/main/java/com/example/dogbreedrecognition/MainActivity.kt
+++ app/src/main/java/com/example/dogbreedrecognition/MainActivity.kt
@@ -1,12 +1,68 @@
 package com.example.dogbreedrecognition
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private lateinit var dogDetector: DogDetector
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        dogDetector = DogDetector(this)
+        dogDetector.view = this
+
+        imageView.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).let {
+                if (it.resolveActivity(packageManager) != null) {
+                    startActivityForResult(it, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        dogDetector.view = null
+        super.onDestroy()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            (data?.extras?.get("data") as Bitmap).apply {
+                Bitmap.createBitmap(this, 0, height / 2 - width / 2, width, width).let {
+                    imageView.setImageBitmap(it)
+                    imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                    dogDetector.recognizeDog(bitmap = it)
+                }
+            }
+        }
+    }
+
+    override fun displayDogBreed(dogBreed: String, winPercent: Float) {
+        textView.text = String.format(
+            Locale.FRANCE, getString(R.string.dog_result), dogBreed,
+            winPercent)
+    }
+
+    override fun displayError() {
+        Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show()
     }
 }
