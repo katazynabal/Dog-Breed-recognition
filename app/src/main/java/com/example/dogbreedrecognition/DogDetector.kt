931971e997787af6cc006ed4c6b973e3bfa7e554
Aurimas Sikorkas <aurimas@dogo.app>
2019-11-29 13:02:33 +0200

    aaaaa

--- app/src/main/java/com/example/dogbreedrecognition/DogDetector.kt
+++ app/src/main/java/com/example/dogbreedrecognition/DogDetector.kt
@@ -2,19 +2,13 @@ package com.example.dogbreedrecognition
 
 import android.content.Context
 import android.graphics.Bitmap
-import com.google.firebase.ml.common.FirebaseMLException
-import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions
-import com.google.firebase.ml.common.modeldownload.FirebaseModelManager
+import android.graphics.Color
+import android.util.Log
 import com.google.firebase.ml.custom.*
-import com.google.firebase.ml.custom.model.FirebaseCloudModelSource
-import com.google.firebase.ml.custom.model.FirebaseLocalModelSource
-import com.google.firebase.ml.custom.model.FirebaseModelDownloadConditions
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 class DogDetector(private val context: Context) {
-
-    private var interpreter: FirebaseModelInterpreter? = null
     private val labels = mutableListOf<String>()
     private lateinit var dataOptions: FirebaseModelInputOutputOptions
 
@@ -28,91 +22,55 @@ class DogDetector(private val context: Context) {
 
     init {
         initializeLabels()
-        initializeDataOptions()
-        initializeInterpreter()
     }
 
-    fun recognizeDog(bitmap: Bitmap) {
-        try {
-            val inputs = FirebaseModelInputs.Builder().add(fromBitmapToByteBuffer(bitmap)).build()
-            interpreter
-                ?.run(inputs, dataOptions)
-                ?.addOnSuccessListener {
-                    val output = it.getOutput<Array<FloatArray>>(0)
-                    val label = labels
-                        .mapIndexed { index, label ->
-                            Pair(label, output[0][index])
-                        }
-                        .sortedByDescending { it.second }
-                        .first()
+    fun doStuff(bitmap: Bitmap) {
+        val localModel = FirebaseCustomLocalModel.Builder()
+            .setAssetFilePath("dog-breed-detector.tflite")
+            .build()
 
-                    //view?.displayDogBreed(label.first, label.second*100)
-                }
-                ?.addOnFailureListener {
-                    //view?.displayError()
-                }
-        } catch (e: FirebaseMLException) {
-            //view?.displayError()
+        val options = FirebaseModelInterpreterOptions.Builder(localModel).build()
+        val interpreter = FirebaseModelInterpreter.getInstance(options)
+        FirebaseModelInterpreterOptions.Builder(localModel).build()
+
+        val inputOutputOptions = FirebaseModelInputOutputOptions.Builder()
+            .setInputFormat(0, FirebaseModelDataType.FLOAT32, intArrayOf(1, 224, 224, 3))
+            .setOutputFormat(0, FirebaseModelDataType.FLOAT32, intArrayOf(1, labels.size))
+            .build()
+
+        val batchNum = 0
+        val input = Array(1) { Array(224) { Array(224) { FloatArray(3) } } }
+        for (x in 0..223) {
+            for (y in 0..223) {
+                val pixel = bitmap.getPixel(x, y)
+                // Normalize channel values to [-1.0, 1.0]. This requirement varies by
+                // model. For example, some models might require values to be normalized
+                // to the range [0.0, 1.0] instead.
+                input[batchNum][x][y][0] = (Color.red(pixel) - 127) / 255.0f
+                input[batchNum][x][y][1] = (Color.green(pixel) - 127) / 255.0f
+                input[batchNum][x][y][2] = (Color.blue(pixel) - 127) / 255.0f
+            }
         }
+
+        val inputs = FirebaseModelInputs.Builder()
+            .add(input) // add() as many input arrays as your model requires
+            .build()
+
+        interpreter?.run(inputs, inputOutputOptions)?.addOnSuccessListener { result ->
+            val output = result.getOutput<Array<FloatArray>>(0)
+            val label = labels
+                .mapIndexed { index, label ->
+                    Pair(label, output[0][index])
+                }.maxBy { it.second }!!
+
+            //view?.displayDogBreed(label.first, label.second*100)
+            Log.e("boom","veikia")
+            }?.addOnFailureListener { e ->
+                Log.e("boom","neveikia Q_Q")
+            }
     }
 
     private fun initializeLabels() {
         labels.addAll(context.assets.open("labels.txt").bufferedReader().readLines())
-    }
-
-    private fun initializeDataOptions() {
-        dataOptions = FirebaseModelInputOutputOptions.Builder()
-            .setInputFormat(0, FirebaseModelDataType.FLOAT32, intArrayOf(1, IMG_SIZE, IMG_SIZE, 3))
-            .setOutputFormat(0, FirebaseModelDataType.FLOAT32, intArrayOf(1, labels.size))
-            .build()
-    }
-
-    private fun initializeInterpreter() {
-        try {
-            val localSource = FirebaseLocalModelSource.Builder(ASSET)
-                .setAssetFilePath("$MODEL_NAME.tflite")
-                .build()
-
-            val conditions = FirebaseModelDownloadConditions.Builder().requireWifi().build()
-            val cloudSource = FirebaseCloudModelSource.Builder(MODEL_NAME)
-                .enableModelUpdates(true)
-                .setInitialDownloadConditions(conditions)
-                .setUpdatesDownloadConditions(conditions)
-                .build()
-
-            FirebaseModelManager.getInstance().apply {
-                registerLocalModelSource(localSource)
-                registerCloudModelSource(cloudSource)
-            }
-
-            interpreter = FirebaseModelInterpreter.getInstance(
-                FirebaseModelOptions.Builder()
-                    .setCloudModelName(MODEL_NAME)
-                    .setLocalModelName(ASSET)
-                    .build()
-            )
-        } catch (e: FirebaseMLException) {
-            //view?.displayError()
-        }
-    }
-
-    private fun fromBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
-        val imgData = ByteBuffer.allocateDirect(4 * IMG_SIZE * IMG_SIZE * 3).apply {
-            order(ByteOrder.nativeOrder())
-            rewind()
-        }
-
-        val pixels = IntArray(IMG_SIZE * IMG_SIZE)
-        Bitmap.createScaledBitmap(bitmap, IMG_SIZE, IMG_SIZE, false).apply {
-            getPixels(pixels, 0, width, 0, 0, width, height)
-        }
-
-        pixels.forEach {
-            imgData.putFloat(((it shr 16 and 0xFF) - MEAN) / STD)
-            imgData.putFloat(((it shr 8 and 0xFF) - MEAN) / STD)
-            imgData.putFloat(((it and 0xFF) - MEAN) / STD)
-        }
-
-        return imgData
     }
 }
--- app/src/main/java/com/example/dogbreedrecognition/MainActivity.kt
+++ app/src/main/java/com/example/dogbreedrecognition/MainActivity.kt
@@ -1,68 +1,122 @@
 package com.example.dogbreedrecognition
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+    lateinit var imageView: ImageView
+    lateinit var captureButton: Button
 
-    private lateinit var dogDetector: DogDetector
-
-    companion object {
-        private const val REQUEST_IMAGE_CAPTURE = 1
-    }
+    val REQUEST_IMAGE_CAPTURE = 1
+    private val PERMISSION_REQUEST_CODE: Int = 101
+    private var mCurrentPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        dogDetector = DogDetector(this)
-        dogDetector.view = this
+        imageView = findViewById(R.id.image_view)
+        captureButton = findViewById(R.id.btn_capture)
+        captureButton.setOnClickListener(View.OnClickListener {
+            if (checkPersmission()) takePicture() else requestPermission()
+        })
+    }
 
-        imageView.setOnClickListener {
-            Intent(MediaStore.ACTION_IMAGE_CAPTURE).let {
-                if (it.resolveActivity(packageManager) != null) {
-                    startActivityForResult(it, REQUEST_IMAGE_CAPTURE)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    takePicture()
+
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
                 }
+                return
+            }
+
+            else -> {
+
             }
         }
     }
 
-    override fun onDestroy() {
-        dogDetector.view = null
-        super.onDestroy()
+    private fun takePicture() {
+
+        val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val file: File = createFile()
+
+        val uri: Uri = FileProvider.getUriForFile(
+            this,
+            "com.example.android.fileprovider",
+            file
+        )
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+
     }
 
+    @SuppressLint("MissingSuperCall")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            (data?.extras?.get("data") as Bitmap).apply {
-                Bitmap.createBitmap(this, 0, height / 2 - width / 2, width, width).let {
-                    imageView.setImageBitmap(it)
-                    imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-                    dogDetector.recognizeDog(bitmap = it)
-                }
-            }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+
+            //To get the File for further usage
+            val auxFile = File(mCurrentPhotoPath)
+
+
+            var bitmap: Bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath)
+            imageView.setImageBitmap(bitmap)
+
         }
     }
 
-    override fun displayDogBreed(dogBreed: String, winPercent: Float) {
-        textView.text = String.format(
-            Locale.FRANCE, getString(R.string.dog_result), dogBreed,
-            winPercent)
+    private fun checkPersmission(): Boolean {
+        return (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) ==
+            PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
     }
 
-    override fun displayError() {
-        Toast.makeText(this, R.string.error, Toast.LENGTH_SHORT).show()
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE, CAMERA), PERMISSION_REQUEST_CODE)
+    }
+
+    @Throws(IOException::class)
+    private fun createFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            mCurrentPhotoPath = absolutePath
+        }
     }
 }
